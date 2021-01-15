4a02bb211843f814875e1f6de47e02cc4ce81b2b
Rostyslav Miniukov <thresh1337@gmail.com>
2021-01-15 12:02:48 +0200

    Обновил снепшот

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -4,9 +4,12 @@ import renderer from "react-test-renderer";
 import WelcomeScreen from "./welcome-screen";
 
 it(`Should WelcomeScreen render correctly`, () => {
+  const onPlayButtonClick = jest.fn();
+
   const tree = renderer
     .create(<WelcomeScreen
       errorsCount={3}
+      onPlayButtonClick={onPlayButtonClick}
     />)
     .toJSON();
 
