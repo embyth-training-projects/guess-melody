92b770edf953929048702cb2f71f244f53317a31
Rostyslav Miniukov <thresh1337@gmail.com>
2021-01-16 20:41:19 +0200

    Добавил состояние и логику отображения игровых экранов в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,17 +2,68 @@ import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+
+import {GameType} from "../../const";
 
 class App extends PureComponent {
-  render() {
-    const {errorsCount} = this.props;
+  static getScreen(question, props, onUserAnswer) {
+    if (question === -1) {
+      const {errorsCount} = props;
 
-    return (
-      <WelcomeScreen
+      return <WelcomeScreen
         errorsCount={errorsCount}
-        onPlayButtonClick={() => {}}
-      />
-    );
+        onPlayButtonClick={onUserAnswer}
+      />;
+    }
+
+    const {questions} = props;
+    const currentQuestion = questions[question];
+
+    switch (currentQuestion.type) {
+      case GameType.GENRE:
+        return <GenreQuestionScreen
+          question={currentQuestion}
+          onAnswerSubmit={onUserAnswer}
+        />;
+      case GameType.ARTIST:
+        return <ArtistQuestionScreen
+          question={currentQuestion}
+          onAnswerSubmit={onUserAnswer}
+        />;
+    }
+
+    return null;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gameQuestionIndex: -1,
+    };
+
+    this._userAnswerHandler = this._userAnswerHandler.bind(this);
+  }
+
+  render() {
+    const {gameQuestionIndex} = this.state;
+
+    return App.getScreen(gameQuestionIndex, this.props, this._userAnswerHandler);
+  }
+
+  _userAnswerHandler() {
+    const {questions} = this.props;
+
+    this.setState((prevState) => {
+      const nextIndex = prevState.gameQuestionIndex + 1;
+      const isEnd = nextIndex >= questions.length;
+
+      return {
+        gameQuestionIndex: isEnd ? -1 : nextIndex
+      };
+    });
   }
 }
 
