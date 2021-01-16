78e71739a597958f5193cb1c0cf5b3a871117926
Rostyslav Miniukov <thresh1337@gmail.com>
2021-01-16 18:56:49 +0200

    Исправил ошибки

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -27,7 +27,7 @@ class GenreQuestionScreen extends PureComponent {
 
           <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
             <circle className="timer__line" cx="390" cy="390" r="370"
-              style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
           </svg>
 
           <div className="game__mistakes">
@@ -61,7 +61,7 @@ class GenreQuestionScreen extends PureComponent {
                       const value = evt.target.checked;
 
                       this.setState({
-                        answers: [...userAnswers.slice(0, index), value, ...userAnswers(index + 1)],
+                        answers: [...userAnswers.slice(0, index), value, ...userAnswers.slice(index + 1)],
                       });
                     }}
                   />
